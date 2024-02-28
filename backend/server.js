2ac183d3e2bfe1ea4f0d706e4ba11b78bd8bec14
Richard Yee <richardyee@192-168-1-5.tpgi.com.au>
2024-02-28 12:04:27 +1000

    backend1 complete

--- backend/server.js
+++ backend/server.js
@@ -5,3 +5,18 @@
 //npm init   server.js
 //npm i axios cors express json-server
 //npm i nodemon concurrently -D
+
+const express = require("express")
+const cors = require("cors")
+const router = require("./routes/index")
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use("/api",router)
+
+const PORT = 80
+app.listen(PORT, function(){
+    console.log("server is running on http://localhost:80")
+})
